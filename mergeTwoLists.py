e95785dea3162ac6e06c4d0bf8dc158aec89befd
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-05-16 22:17:01 -0500

    __str__

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -5,3 +5,11 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+   def __str__(self):
+        # Helper method for printing linked list
+        result = []
+        current = self
+        while current:
+            result.append(str(current.val))
+            current = current.next
+        return "->".join(result)
